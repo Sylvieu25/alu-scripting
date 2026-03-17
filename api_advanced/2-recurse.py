b1fb61539e3fa55b6022ae964d86de3c53ff26b0
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-17 18:23:02 +0000

    Fix api_advanced from submodule to regular directory

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module to recursively query the Reddit API and return a list
+of titles of all hot articles for a given subreddit."""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Recursively query the Reddit API and return a list of titles
+    of all hot articles for a given subreddit.
+    Returns None if invalid subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+        hot_list (list): Accumulated list of hot article titles.
+        after (str): Pagination token for next page.
+
+    Returns:
+        list: List of hot article titles, or None if invalid.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "linux:api_advanced:v1.0 (by /u/api_advanced)"}
+    params = {"limit": 100}
+
+    if after:
+        params["after"] = after
+
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+    after = data.get("data", {}).get("after")
+
+    for post in posts:
+        hot_list.append(post.get("data", {}).get("title"))
+
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, after)
