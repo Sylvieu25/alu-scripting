cd3e80edbcc41f010752cecccab73ac29b332841
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-26 09:42:31 +0000

    Add executable 1-top_ten.py

---
+++ 1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module to query the Reddit API and print the top 10 hot posts
+for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Query the Reddit API and print the top 10 hot posts
+    for a given subreddit. Prints None if invalid subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        "User-Agent": "python:api_advanced:v1.0 (by /u/api_advanced_user)"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+
+    for post in posts:
+        print(post.get("data", {}).get("title"))
